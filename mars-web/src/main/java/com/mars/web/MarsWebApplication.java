0ed518f148a90d4432ef82d48cac9afb81c19dd5
Mars <tngus9536@gmail.com>
2026-02-06 10:55:31 +0900

    Initial mars-web project

---
+++ mars-web/src/main/java/com/mars/web/MarsWebApplication.java
@@ -0,0 +1,13 @@
+package com.mars.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsWebApplication.class, args);
+	}
+
+}
