5761ed01346eb982322735a5e6ee38a6d49aa844
Mars <tngus9536@gmail.com>
2026-02-12 15:14:59 +0900

    주석 추가 및 필요 없는 파일 삭제

--- mars-web/src/main/java/com/mars/web/core/util/FileUtil.java
+++ mars-web/src/main/java/com/mars/web/core/util/FileUtil.java
@@ -1,5 +1,16 @@
 package com.mars.web.core.util;
 
+/**
+ * FileUtil
+ *
+ * 파일 업로드/다운로드 Util 클래스
+ *
+ * History
+ * - 2026.02.12 : 최초 생성.
+ *
+ * @author Mars
+ * @since 2026.02.12
+ */
 public class FileUtil {
 
 }
--- mars-web/src/main/java/com/mars/web/core/util/PropertiesUtil.java
+++
@@ -1,5 +0,0 @@
-package com.mars.web.core.util;
-
-public class PropertiesUtil {
-
-}
